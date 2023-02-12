9df98f90cc50d316d6420d403d059fa33ad6ee13
kiccho1101 <youodf11khp@gmail.com>
2023-02-12 00:04:06 +0000

    update demo

--- kaggle_otto2/cand_generator/item2vec/cand_generator.py
+++ kaggle_otto2/cand_generator/item2vec/cand_generator.py
@@ -3,7 +3,8 @@ from pathlib import Path
 
 import polars as pl
 
-from kaggle_otto2.cand_generator.base.cand_generator_base import CandGeneratorBase
+from kaggle_otto2.cand_generator.base.cand_generator_base import \
+    CandGeneratorBase
 from kaggle_otto2.cand_generator.item2vec.enums import SeedType
 from kaggle_otto2.cand_generator.item2vec.trainer import Item2VecTrainer
 from kaggle_otto2.config import Config
@@ -19,7 +20,9 @@ class Item2VecCandGenerator(CandGeneratorBase):
         data_loader: OttoDataLoader,
         seed_type: SeedType,
     ):
-        cg_name = "item2vec"
+        self.short_cg_name = "item2vec"
+        self.seed_type = seed_type
+        cg_name = f"{self.short_cg_name}_{self.seed_type}"
         super().__init__(
             root_dir=root_dir,
             data_loader=data_loader,
@@ -29,9 +32,8 @@ class Item2VecCandGenerator(CandGeneratorBase):
             dtype=pl.Float32,
         )
         self.trainer = Item2VecTrainer(data_loader=data_loader, config=config)
-        self.model_dir = self.data_dir
+        self.model_dir = root_dir / "cand_generator" / self.short_cg_name
         self.gen_cand_topk = config.yaml.cg[cg_name].get("gen_cand_topk", 20)
-        self.seed_type = seed_type
 
     def fit(self):
         self.trainer.fit(self.model_dir)
